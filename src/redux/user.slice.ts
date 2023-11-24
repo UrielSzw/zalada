8726d81b9a44b79d6c6e3db29a5ec9bc91bdb4cb
Facundo Colombier <fcolombier@teravisiontech.com>
2023-11-24 12:44:01 -0300

    Build error

--- src/redux/user.slice.ts
+++ src/redux/user.slice.ts
@@ -18,33 +18,29 @@ const initialState: UserState = {
 
 const initialStates = {
   userData: {
-    email: "jdoe@example.com",
-    firstname: "Jane",
-    lastname: "Doe",
+    email: 'jdoe@example.com',
+    firstname: 'Jane',
+    lastname: 'Doe',
     addresses: [
       {
         defaultShipping: true,
         defaultBilling: true,
-        firstname: "Jane",
-        lastname: "Doe",
+        firstname: 'Jane',
+        lastname: 'Doe',
         region: {
-          regionCode: "NY",
-          region: "New York",
-          regionId: 43
+          regionCode: 'NY',
+          region: 'New York',
+          regionId: 43,
         },
         postcode: 10755,
-        street: [
-          "123 Oak Ave"
-        ],
-        city: "Purchase",
-        telephone: "512-555-1111",
-        countryId: "US"
-      }
-    ]
+        street: ['123 Oak Ave'],
+        city: 'Purchase',
+        telephone: '512-555-1111',
+        countryId: 'US',
+      },
+    ],
   },
-  password: "Password1"
-
-  },
+  password: 'Password1',
 };
 
 export const userSlice = createSlice({
