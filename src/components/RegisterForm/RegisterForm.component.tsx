f938b17e8b177d3440d7c884bd6622bbfdaac23d
Brandon <trianab659@gmail.com>
2023-11-27 14:24:42 -0500

    added margin botton at error labels

--- src/components/RegisterForm/RegisterForm.component.tsx
+++ src/components/RegisterForm/RegisterForm.component.tsx
@@ -27,27 +27,27 @@ export const RegisterForm: React.FC<Props> = ({
         </StyledText>
       </View>
       <View style={styles.contentWrapper}>
-        <FormikTextInput name="firstName" placeholder="First Name" marginBottomError={0} />
-        <FormikTextInput name="lastName" placeholder="Last Name" marginBottomError={0} />
+        <FormikTextInput name="firstName" placeholder="First Name" marginBottomError={5} />
+        <FormikTextInput name="lastName" placeholder="Last Name" marginBottomError={5} />
         <FormikTextInput
           name="email"
           placeholder="E-mail"
           inputMode="email"
-          marginBottomError={0}
+          marginBottomError={5}
         />
         <FormikTextInput
           name="password"
           placeholder="Password"
           secureTextEntry
-          marginBottomError={0}
+          marginBottomError={5}
         />
         <View style={{ display: 'flex', flexDirection: 'row' }}>
-          <FormikTextInput name="region" placeholder="City" marginBottomError={0} />
+          <FormikTextInput name="region" placeholder="City" marginBottomError={5} />
           <View style={{ width: 20 }} />
-          <FormikTextInput name="postCode" placeholder="Postal Code" marginBottomError={0} />
+          <FormikTextInput name="postCode" placeholder="Postal Code" marginBottomError={5} />
         </View>
-        <FormikTextInput name="street" placeholder="Street" marginBottomError={0} />
-        <FormikTextInput name="telephone" placeholder="Telephone" marginBottomError={0} />
+        <FormikTextInput name="street" placeholder="Street" marginBottomError={5} />
+        <FormikTextInput name="telephone" placeholder="Telephone" marginBottomError={5} />
         <View style={styles.buttonsWrapper}>
           <View style={styles.signupLink}>
             <StyledText color="white">Already registered? </StyledText>
