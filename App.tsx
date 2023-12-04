0f3a3888752fb60761568e1303842f3bf0ecdeb3
Uriel <urielszw@gmail.com>
2023-12-04 17:01:51 -0300

    ZAL-20: fix

--- App.tsx
+++ App.tsx
@@ -4,23 +4,20 @@ import Routes from './src/routes/routes';
 import { theme } from './src/theme';
 import { Provider } from 'react-redux';
 import { store } from './src/redux';
-import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const App = (): JSX.Element => {
   const queryClient = new QueryClient();
 
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.primary }}>
-            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
-            <Routes />
-          </SafeAreaView>
-        </Provider>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.primary }}>
+          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
+          <Routes />
+        </SafeAreaView>
+      </Provider>
+    </QueryClientProvider>
   );
 };
 
