10efea59a073fe7e92de7284adfd47d031594ac3
emilio_ledo <fernandezledoemilio@gmail.com>
2023-12-05 15:23:39 -0300

    Find new png for defualt profile picture

--- src/components/UI/Avatar/Avatar.component.tsx
+++ src/components/UI/Avatar/Avatar.component.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Image, ImageSourcePropType } from 'react-native';
 import { getStyles } from './Avatar.styles';
-import { avatarImage } from '../../../assets';
+import { userImage } from '../../../assets';
 
 type Props = {
   img?: ImageSourcePropType;
   big?: boolean;
 };
 
-export const Avatar: React.FC<Props> = ({ img = avatarImage, big }) => {
+export const Avatar: React.FC<Props> = ({ img = userImage, big }) => {
   const styles = getStyles({ big });
 
   return <Image style={styles.avatar} source={img} />;
