d90b62e9641f47f754ad92a3c2d43adc0aa12c30
Alex Teper <67521917+AlexTeper99@users.noreply.github.com>
2023-12-11 11:50:11 -0300

    zal-19/fix-layout

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import Routes from './src/routes/routes';
 import { theme } from './src/theme';
 import { Provider } from 'react-redux';
 import { store } from './src/redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App = (): JSX.Element => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.primary }}>
-          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
-          <Routes />
-        </SafeAreaView>
-      </Provider>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.primary }}>
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
+            <Routes />
+          </SafeAreaView>
+        </Provider>
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 };
