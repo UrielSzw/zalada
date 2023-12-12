d395497d762b1df2c04616c781e3cd62dc67ad95
emilio_ledo <fernandezledoemilio@gmail.com>
2023-12-12 14:29:03 -0300

    update coments

--- src/components/AddAdressForm/AddAdressForm.component.tsx
+++ src/components/AddAdressForm/AddAdressForm.component.tsx
@@ -9,7 +9,6 @@ type Props = {
   handleOnSubmitUpdateAddress: any;
   errors: any;
   control: any;
-  navigation: any;
 };
 
 export const AddAdressForm: React.FC<Props> = ({
@@ -27,95 +26,89 @@ export const AddAdressForm: React.FC<Props> = ({
         control={control}
         errors={errors}
         rules={{
-          required: 'firstname is required',
+          required: 'Firstname is required',
           pattern: {
             value: /^[a-zA-Z0-9]{3,20}$/,
             message:
-              'firstname must be between 3 and 20 characters long and can only contain letters and numbers.',
+              'Firstname must be between 3 and 20 characters long and can only contain letters and numbers.',
           },
         }}
         style={styles.input}
         placeholder="Firstname"
-        autoCapitalize="none"
       />
       <FormInput
         name="lastname"
         control={control}
         errors={errors}
         rules={{
-          required: 'lastname is required',
+          required: 'Lastname is required',
           pattern: {
             value: /^[a-zA-Z0-9]{3,20}$/,
             message:
-              'lastname must be between 3 and 20 characters long and can only contain letters and numbers.',
+              'Lastname must be between 3 and 20 characters long and can only contain letters and numbers.',
           },
         }}
         style={styles.input}
         placeholder="Lastname"
-        autoCapitalize="none"
       />
       <FormInput
         name="region"
         control={control}
         errors={errors}
         rules={{
-          required: 'city is required',
+          required: 'City is required',
           pattern: {
             value: /^[a-zA-Z0-9]{3,20}$/,
             message:
-              'city must be between 3 and 8 characters long and can only contain letters and numbers.',
+              'City must be between 3 and 20 characters long and can only contain letters and numbers.',
           },
         }}
         style={styles.input}
         placeholder="City"
-        autoCapitalize="none"
       />
       <FormInput
         name="postcode"
         control={control}
         errors={errors}
         rules={{
-          required: 'postcode is required',
+          required: 'Postcode is required',
           pattern: {
             value: /^[a-zA-Z0-9]{3,20}$/,
             message:
-              'postcode must be between 3 and 8 characters long and can only contain letters and numbers.',
+              'Postcode must be between 3 and 20 characters long and can only contain letters and numbers.',
           },
         }}
         style={styles.input}
         placeholder="Postcode"
-        autoCapitalize="none"
       />
       <FormInput
         name="street"
         control={control}
         errors={errors}
         rules={{
-          required: 'street is required',
+          required: 'Street is required',
           pattern: {
             value: /^[a-zA-Z0-9]{3,30}$/,
             message:
-              'postcode must be between 3 and 30 characters long and can only contain letters and numbers.',
+              'Street must be between 3 and 30 characters long and can only contain letters and numbers.',
           },
         }}
         style={styles.input}
         placeholder="Street"
-        autoCapitalize="none"
       />
       <FormInput
         name="telephone"
         control={control}
         errors={errors}
         rules={{
-          required: 'telephone is required',
+          required: 'Telephone is required',
           pattern: {
             value: /^\d{8}$/,
-            message: 'telephone must consist of exactly 8 numeric digits.',
+            message: 'Telephone must consist of exactly 8 numeric digits.',
           },
         }}
         style={styles.input}
         placeholder="Telephone"
-        autoCapitalize="none"
       />
       <Pressable onPress={handleSubmit(handleOnSubmitUpdateAddress)} style={styles.updateBtn}>
         <StyledText color="white" weight="bold">
